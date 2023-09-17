6b4d1c994c71f01221515cf29dc11b3f6007b8f1
TarunjeetTJ <144499033+TarunjeetTJ@users.noreply.github.com>
2023-09-17 12:40:33 +0530

    Time: 1 ms (100.00%) | Memory: 44.6 MB (51.02%) - LeetSync

--- 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.java
+++ 2043-cyclically-rotating-a-grid/cyclically-rotating-a-grid.java
@@ -1,34 +1,78 @@
 class Solution {
     public int[][] rotateGrid(int[][] grid, int k) {
-        int ktemp=k;
-        int rmin=0;
-        int rmax=grid.length-1;
-        int cmin=0;
-        int cmax=grid[0].length-1;
-        while(rmin<rmax && cmin<cmax){
-            int elements=((rmax-rmin+1)+(cmax-cmin-1))*2;
-            ktemp=k%elements;
-            for(int i=0;i<ktemp;i++){
-                int temp=grid[rmin][cmin];
-                for(int j=cmin;j<cmax;j++){
-                    grid[rmin][j]=grid[rmin][j+1];
-                }
-                for(int j=rmin;j<rmax;j++){
-                    grid[j][cmax]=grid[j+1][cmax];
-                }
-                for(int j=cmax;j>cmin;j--){
-                    grid[rmax][j]=grid[rmax][j-1];
-                }
-                for(int j=rmax;j>rmin;j--){
-                    grid[j][cmin]=grid[j-1][cmin];
-                }
-                grid[rmin+1][cmin]=temp;
-            }
-            rmin++;
-            cmin++;
-            rmax--;
-            cmax--;
+        int n = grid.length;
+        int m = grid[0].length;
+        int times = Math.min(n,m)/2;
+        for(int i=1;i<=times;i++){
+            rotateShell(grid,i,k);
         }
         return grid;
+    }
+    public static void rotateShell(int[][] mat, int s, int r){
+        int oned[] = fillOnedFromShell(mat,s);
+        rotate(oned,r);
+        fillShellFromOned(mat,s,oned);
+    }
+    public static int[] fillOnedFromShell(int[][] mat, int s){
+        int maxR = mat.length - s;
+        int maxC = mat[0].length - s;
+        int minR = s-1;
+        int minC = s-1;
+        int size = 2*(maxR-minR+maxC-minC);
+        int oned[] = new int[size];
+        int idx=0;
+        for(int i=minR,j=minC;i<=maxR;i++){
+            oned[idx++] = mat[i][j];
+        }
+        for(int i=maxR,j=minC+1;j<=maxC;j++){
+            oned[idx++] = mat[i][j];
+        }
+        for(int i=maxR-1,j=maxC;i>=minR;i--){
+            oned[idx++] = mat[i][j];
+        }
+        for(int i=minR,j=maxC-1;j>minC;j--){
+            oned[idx++] = mat[i][j];
+        }
+        return oned;
+    }
+
+    public static void fillShellFromOned(int[][] mat, int s, int[] oned){
+        int maxR = mat.length - s;
+        int maxC = mat[0].length - s;
+        int minR = s-1;
+        int minC = s-1;
+        int size = 2*(maxR-minR+maxC-minC);
+        int idx=0;
+        for(int i=minR,j=minC;i<=maxR;i++){
+            mat[i][j] = oned[idx++];
+        }
+        for(int i=maxR,j=minC+1;j<=maxC;j++){
+            mat[i][j] = oned[idx++];
+        }
+        for(int i=maxR-1,j=maxC;i>=minR;i--){
+            mat[i][j] = oned[idx++];
+        }
+        for(int i=minR,j=maxC-1;j>minC;j--){
+            mat[i][j] = oned[idx++];
+        }
+    }
+    public static void rotate(int[] oned,int r){
+        int n = oned.length;
+        r = r%n;
+        if(r<0){
+            r=n+r;
+        }
+        reverse(oned,0,n-r-1);
+        reverse(oned,n-r,n-1);
+        reverse(oned,0,n-1);
+    }
+    public static void reverse(int oned[], int l, int r){
+        while(l<r){
+            int tmp = oned[l];
+            oned[l] = oned[r];
+            oned[r] = tmp;
+            l++;
+            r--;
+        }
     }
 }
